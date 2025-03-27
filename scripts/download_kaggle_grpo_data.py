e3d4424832c1aa61f9eac49adfaff04b605b6c73
hav4ik <hav4ik@users.noreply.github.com>
2025-03-27 06:39:17 -0700

    got

--- scripts/download_kaggle_grpo_data.py
+++ scripts/download_kaggle_grpo_data.py
@@ -3,12 +3,12 @@ import os
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("--model", type=str, required=True)
-parser.add_argument("--data", type=str, required=True)
-parser.add_argument("--output_dir", type=str, required=True)
+parser.add_argument("--model", type=str, required=False)
+parser.add_argument("--data", type=str, required=False)
 args = parser.parse_args()
-os.makedirs(args.output_dir, exist_ok=True)
 
 # Download a single file.
-kagglehub.dataset_download(args.data)
-kagglehub.model_download(args.model)
+if args.data:
+    kagglehub.dataset_download(args.data)
+if args.model:
+    kagglehub.model_download(args.model)
